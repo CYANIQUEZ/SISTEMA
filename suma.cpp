f23b287df2dc38c4035d93109d256f6fc62dc4f5
CYANIQUEZ <112488629+CYANIQUEZ@users.noreply.github.com>
2022-08-31 19:20:37 -0400

    Add files via upload

---
+++ suma.cpp
@@ -0,0 +1,16 @@
+#include<iostream>  // libreria
+using namespace std;
+main()
+{
+  int a;
+   int b,c=0;   // declaracion de variables  
+	cout<<"ingrese a.."; //salida
+	cin>>a;	//entrada
+	
+	cout<<"ingrese b.."; //salida
+	cin>>b;	//entrada
+	
+	c=a+b;
+	cout<<"la suma es... "<<c;
+	
+}
